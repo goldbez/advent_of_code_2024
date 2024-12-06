14dda7313b220c44f871fa14e5c8646d0a05a207
Zachary Goldberg <goldbez@amazon.com>
2024-12-05 21:48:49 -0800

    day 5 optimizations

--- day_5/solution.py
+++ day_5/solution.py
@@ -5,9 +5,9 @@ def get_anteriors(orders):
 	anteriors = dict()
 	for before, after in orders:
 		if after in anteriors:
-			anteriors[after].append(before)
+			anteriors[after].add(before)
 		else:
-			anteriors[after] = [before]
+			anteriors[after] = {before}
 	return anteriors
 
 
@@ -21,33 +21,28 @@ def get_middle_if_in_order(update, anteriors):
 	return update[len(update)//2]
 
 
-def reorder(update, anteriors):
+def reorder_update(update, anteriors):
 	keep_going = True
 	while keep_going:
 		keep_going = False
 		for i in range(len(update)):
 			if update[i] in anteriors:
-				last_anterior_index = -1
-				for j in range(i+1, len(update)):
+				for j in range(len(update)-1, i, -1):
 					if update[j] in anteriors[update[i]]:
-						last_anterior_index = j
-				if last_anterior_index != -1:
-					to_insert = update.pop(i)
-					update.insert(last_anterior_index, to_insert)
-					keep_going = True
-					break
+						to_insert = update.pop(i)
+						update.insert(j, to_insert)
+						keep_going = True
+						break
 	return update
 
 
-def part1(orders, updates):
-	anteriors = get_anteriors(orders)
+def part1(anteriors, updates):
 	return sum(get_middle_if_in_order(update, anteriors) for update in updates)
 
 
-def part2(orders, updates):
-	anteriors = get_anteriors(orders)
+def part2(anteriors, updates):
 	incorrect_updates = [update for update in updates if get_middle_if_in_order(update, anteriors) == 0]
-	reordered_updates = [reorder(update, anteriors) for update in incorrect_updates]
+	reordered_updates = [reorder_update(update, anteriors) for update in incorrect_updates]
 	return sum(update[len(update)//2] for update in reordered_updates)
 
 
@@ -57,8 +52,9 @@ def main():
 	raw_orders, raw_updates = raw.strip().split('\n\n')
 	orders = [[int(j) for j in i.split('|')] for i in raw_orders.split('\n')]
 	updates = [[int(j) for j in i.split(',')] for i in raw_updates.split('\n')]
-	print(part1(orders, updates))
-	print(part2(orders, updates))
+	anteriors = get_anteriors(orders)
+	print(part1(anteriors, updates))
+	print(part2(anteriors, updates))
 
 
 if __name__ == '__main__':
